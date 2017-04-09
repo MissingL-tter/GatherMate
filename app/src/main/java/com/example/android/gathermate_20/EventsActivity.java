8b2235f66f1152a95139260a3276067320701af4
MissingL-tter <434a44@gmail.com>
2017-04-09 13:11:17 -0400

    Adding search functionality to AddFriend button on App Bar. Added ability to search for a friend by email Stored the found friend userdb/uid/friends Added icons for Add Friends and Friends List Requires polish.

--- app/src/main/java/com/example/android/gathermate_20/EventsActivity.java
+++ app/src/main/java/com/example/android/gathermate_20/EventsActivity.java
@@ -1,19 +1,26 @@
 package com.example.android.gathermate_20;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,8 +35,12 @@ public class EventsActivity extends AppCompatActivity implements ActivityCompat.
 
     private static final String TAG = "EVENTS";
 
+    private final Activity context = this;
+
     ListView listViewEvents;
+    String uid;
     DatabaseReference databaseEvents;
+    DatabaseReference databaseUsers;
     List<Event> eventList;
 
     @Override
@@ -51,6 +62,8 @@ public class EventsActivity extends AppCompatActivity implements ActivityCompat.
             }
         });
 
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        databaseUsers = FirebaseDatabase.getInstance().getReference().child("userdb");
         databaseEvents = FirebaseDatabase.getInstance().getReference().child("eventdb");
         eventList = new ArrayList<>();
 
@@ -109,31 +122,69 @@ public class EventsActivity extends AppCompatActivity implements ActivityCompat.
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_events_app_bar, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.socialFindFriends);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("Find Users by Email...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String emailQuery) {
+                databaseUsers.orderByChild("info/email").equalTo(emailQuery).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()){
+                            for(DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                                //TODO: Create prompt showing name of user and verifying adding them to friends
+                                //PopupWindow or something similar
+                                //For now the searchItem view is collapsing only if the user was found and added as a friend
+                                databaseUsers.child(uid).child("friends").child(userSnapshot.getKey()).child("name").setValue(userSnapshot.child("info").child("name").getValue().toString());
+                                searchItem.collapseActionView();
+                            }
+                        }else {
+                            //TODO: Create prompt indicating User Not Found
+                            //PopupWindow or something similar
+                            //For now searchItem view stays open if the user was not found
+                            Log.e(TAG,"SEARCH: User Not Found");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                });
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.socialFindFriends:
-                System.out.println("ADD_FRIENDS_CLICKED");
-                return true;
-
             case R.id.socialFriendsList:
+                //TODO: Implement Friends List
+                //This could be an entire activity, a fragment, or a ListView in a PopupWindow
                 System.out.println("FRIENDS_LIST_CLICKED");
                 return true;
 
             case R.id.appBarSettings:
+                //TODO: Implement Settings Activity
+                //Manual XML coding to make the cog icon generate a list
+                //Talk to Chris before proceeding
                 System.out.println("SETTINGS_CLICKED");
                 return true;
 
             default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
         }
     }
