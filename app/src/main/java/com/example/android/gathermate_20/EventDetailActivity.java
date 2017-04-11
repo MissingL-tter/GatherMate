7fa5b770e6d4108c969b7443cf9ad6f6d083705b
thunte32 <thunte32@uncc.edu>
2017-04-10 21:36:07 -0400

    Added button to start navigation intent

--- app/src/main/java/com/example/android/gathermate_20/EventDetailActivity.java
+++ app/src/main/java/com/example/android/gathermate_20/EventDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.gathermate_20;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -26,6 +27,8 @@ public class EventDetailActivity extends AppCompatActivity {
     TextView descriptionView;
     TextView nameView;
     Button deleteButton;
+    Button navigateButton;
+
 
     private DatabaseReference databaseEvents;
 
@@ -87,6 +90,21 @@ public class EventDetailActivity extends AppCompatActivity {
         }
 
         databaseEvents = FirebaseDatabase.getInstance().getReference();
+
+        //Navigate
+        navigateButton = (Button) findViewById(R.id.navigateToEventButton);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double lat = event.lat;
+                double lng = event.lng;
+
+                Uri gmmIntentUri = Uri.parse("google.navigation:q="+String.valueOf(lat)+","+String.valueOf(lng));
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
     }
 
     public void deleteEvent (View v, String uid, String eventId) {
